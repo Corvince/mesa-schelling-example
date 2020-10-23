fa0f0f9f3e299568e3ff5901657be55a1a264425
Corvince <13568919+Corvince@users.noreply.github.com>
2020-10-23 11:23:34 +0200

    Make ready for heroku deployment
    * add procfile
    
    * fix model
    
    * update requirements

--- server.py
+++ server.py
@@ -1,22 +1,23 @@
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid, ChartModule, TextElement
+from mesa.visualization.UserParam import UserSettableParameter
 
 from model import SchellingModel
 
 
 class HappyElement(TextElement):
-    '''
+    """
     Display a text count of how many happy agents there are.
-    '''
+    """
 
     def render(self, model):
         return "Happy agents: " + str(model.happy)
 
 
 def schelling_draw(agent):
-    '''
+    """
     Portrayal Method for canvas
-    '''
+    """
     if agent is None:
         return
     portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 0}
@@ -27,11 +28,24 @@ def schelling_draw(agent):
         portrayal["Color"] = "Blue"
     return portrayal
 
+
 happy_element = HappyElement()
 canvas_element = CanvasGrid(schelling_draw, 20, 20, 500, 500)
 happy_chart = ChartModule([{"Label": "happy", "Color": "Black"}])
 
-server = ModularServer(SchellingModel,
-                       [canvas_element, happy_element, happy_chart],
-                       "Schelling’s Segregation Model",
-                       20, 20, 0.8, 0.2, 4)
+model_params = {
+    "height": 20,
+    "width": 20,
+    "density": UserSettableParameter("slider", "Agent density", 0.8, 0.1, 1.0, 0.1),
+    "minority_pc": UserSettableParameter(
+        "slider", "Fraction minority", 0.2, 0.00, 1.0, 0.05
+    ),
+    "homophily": UserSettableParameter("slider", "Homophily", 3, 0, 8, 1),
+}
+
+server = ModularServer(
+    SchellingModel,
+    [canvas_element, happy_element, happy_chart],
+    "Schelling",
+    model_params,
+)
